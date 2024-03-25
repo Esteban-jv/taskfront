f3f93fd93ceb6ecb5b6075d402397b948179bb39
Estebanjvj <zaorylight@gmail.com>
2024-03-24 19:29:36 -0500

    First commit (FrontEnd)

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
-
+// console.log(process.env);
 const clienteAxios = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    // baseURL: process.env.REACT_APP_BACKEND_URL // now is premium
+    // baseURL: "https://mern-djangorest-server.herokuapp.com/"
+    baseURL: "http://127.0.0.1:8000/"
 });
 
 export default clienteAxios;
